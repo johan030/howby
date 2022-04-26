d6e2a43eaa882e62195da558a4f673e92c5a9a67
johan030 <weylin030@protonmail.ch>
2022-04-26 15:30:18 +0200

    header almost ok

--- components/home/Header.js
+++ components/home/Header.js
@@ -1,30 +1,38 @@
-import { View, Text, StyleSheet, Pressable, Image, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
 
 const Header = () => {
   return (
     <>
       <View style={styles.container}>
-        <Text>retour</Text>
+        <Text>Retour</Text>
         <Text>Sentier des gorges du Verdon</Text>
 
         <View style={styles.wrapper}>
-        <Pressable style={styles.ButtonRed}>
-          <Text style={{color: "white", padding: '5px'}}>Favori</Text>
-        </Pressable>
-        <Pressable style={styles.ButtonBlue}>
-          <Text style={{color: "white"}}>Partager</Text>
-        </Pressable>
-        <Pressable style={styles.ButtonBlue}>
-          <Text style={{color: "white"}}>Appeler</Text>
-        </Pressable>
-        <TouchableOpacity>
-        <Image source = {{ uri: 'https://img.icons8.com/ios/50/000000/download--v1.png'}} style = {styles.icon}/>
-        </TouchableOpacity>
+          <View style={styles.wrapper}>
+            <Pressable style={styles.ButtonRed}>
+              <Text style={{color: 'white'}}>Favori</Text>
+            </Pressable>
+            <Pressable style={styles.ButtonBlue}>
+              <Text style={{color: 'white'}}>Partager</Text>
+            </Pressable>
+            <Pressable style={styles.ButtonBlue}>
+              <Text style={{color: 'white'}}>Appeler</Text>
+            </Pressable>
+          </View>
+          <TouchableOpacity>
+          <Image source = {{ uri: 'https://img.icons8.com/ios/50/000000/download--v1.png'}}
+          style = {styles.icon}/>
+          </TouchableOpacity>
         </View>
-  
       </View>
-      
     </>
   );
 };
@@ -32,32 +40,29 @@ const Header = () => {
 const styles = StyleSheet.create({
   container: {
     marginLeft: "1rem",
-    
-  },
-  wrapper:{
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginTop: '1rem'
-  },
-  ButtonRed:{
-    backgroundColor: 'red',
-    color: "white", //fonctionne pas
-    borderRadius: '.5rem',
     marginRight: '1rem'
-        
-    
+  },
+  wrapper: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: "1rem",
+  },
+  ButtonRed: {
+    backgroundColor: "red",
+    color: "white", //fonctionne pas
+    borderRadius: ".5rem",
+    marginRight: "1rem",
   },
   ButtonBlue: {
     backgroundColor: "#000D4E",
-    color: "#fff", // fonctionne pas? 
-    marginRight: '1rem'
-    
+    color: "#fff", // fonctionne pas?
+    marginRight: "1rem",
   },
   icon: {
     width: 30,
     height: 30,
     marginLeft: 10,
-    resizeMode: 'contain'
+    resizeMode: "contain",
   },
 });
 export default Header;
