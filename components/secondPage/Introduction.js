fe849f91165ce5937b280d3d511cb9b58f057c60
johan030 <weylin030@protonmail.ch>
2022-04-27 15:06:08 +0200

    adding introduction area

--- components/secondPage/Introduction.js
+++ components/secondPage/Introduction.js
@@ -1,11 +1,11 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 const introduction = () => {
   return (
     <View>
-      <Text>DANS</Text>
-      <Text>le livre de réservation, les noms des clients sont rares. "Nous avons deux tables réservées pour midi,
+      <Text style={{fontSize: '25px'}}>DANS</Text>
+      <Text style={styles.text}>le livre de réservation, les noms des clients sont rares. "Nous avons deux tables réservées pour midi,
           trois pour ce soir, consulte Paula.
           Disons que c'est rien pour une journée normale. On va dire que c'est une petite reprise
       </Text>
@@ -14,4 +14,14 @@ const introduction = () => {
   )
 }
 
+
+const styles = StyleSheet.create ({
+    text: {
+        color: '#506266',
+        marginTop: 5,
+        marginBottom: 10,
+        lineHeight: '20px'
+  
+    }
+})
 export default introduction
