e9325c33ba84d865f90bbc912d47ba1e2e79e562
johan030 <weylin030@protonmail.ch>
2022-04-27 10:51:31 +0200

    done services section

--- components/home/Services.js
+++ components/home/Services.js
@@ -9,19 +9,27 @@ const Services = () => {
         <Image source={{uri: "https://img.icons8.com/ios/50/000000/book.png"}} style={styles.icon} />
         </View>
         <View style={styles.dontknowhowtocallmyclass}>
-        <Text style={{color: 'red' }}>équipement et services</Text>
-        <Image source={{uri: "https://img.icons8.com/ios/50/000000/book.png"}} style={styles.icon} />
+        <Text style={{color: 'lightgray'}}>Accès pour personnes à mobilité réduite</Text>
+        <Image source={{uri: "https://img.icons8.com/ios/50/000000/wheelchair.png"}} style={styles.icon} />
         </View>
         <View style={styles.dontknowhowtocallmyclass}>
-        <Text style={{color: 'red' }}>équipement et services</Text>
-        <Image source={{uri: "https://img.icons8.com/ios/50/000000/book.png"}} style={styles.icon} />
+        <Text style={{color: 'lightgray' }}>Animaux acceptés</Text>
+        <Image source={{uri: "https://img.icons8.com/ios/50/000000/dog--v1.png"}} style={styles.icon} />
         </View>
         <View style={styles.dontknowhowtocallmyclass}>
-        <Text style={{color: 'red' }}>équipement et services</Text>
-        <Image source={{uri: "https://img.icons8.com/ios/50/000000/book.png"}} style={styles.icon} />
+        <Text style={{color: 'red' }}>parking gratuit sur place</Text>
+        <Image source={{uri: "https://img.icons8.com/ios-glyphs/30/000000/park-and-charge.png"}} style={styles.icon} />
+        </View>
+        <View style={styles.dontknowhowtocallmyclass}>
+        <Text style={{color: 'black' }}>Français</Text>
+        <Image source={{uri: "https://img.icons8.com/color/48/000000/france.png"}} style={styles.icon} />
+        </View>
+        <View style={styles.dontknowhowtocallmyclass}>
+        <Text style={{color: 'black' }}>Anglais</Text>
+        <Image source={{uri: "https://img.icons8.com/office/16/000000/great-britain.png"}} style={styles.icon} />
         </View>
         <TouchableOpacity>
-            <Text>En savoir plus </Text>
+            <Text style={{marginTop: '10px'}}>En savoir plus </Text>
         </TouchableOpacity>
       
     </View>
@@ -35,12 +43,13 @@ const styles = StyleSheet.create ({
         marginRight: '1rem'
     },
     icon:{
-        width: '30px',
-        height: '30px'
+        width: '20px',
+        height: '20px'
     },
     dontknowhowtocallmyclass:{
         flexDirection: 'row',
-        justifyContent:'space-between'
+        justifyContent:'space-between',
+        marginTop: '15px'
     }
 })
 
